ef4faebc9282f94ecebbc572e8efad240ff97b5a
Seamus O'Sullivan <info@seamuso.com>
2021-02-05 10:55:41 +0100

    initial

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var today = new Date();
+var dd = String(today. getDate()). padStart(2, '0');
+var mm = String(today. getMonth() + 1). padStart(2, '0'); //January is 0!
+var yyyy = today. getFullYear();
+​
+today = mm + '/' + dd + '/' + yyyy;
+document. write(today);
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: [String],
+  cuisine: {type: String, required: true},
+  dishType: { type: String, enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'] },
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number, min: 0 },
+  creator: String,
+  created: Date
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
